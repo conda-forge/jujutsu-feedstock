b36cd461220c3d596c00c50da3209e700937fed5
Joshua L. Adelman <joshua.adelman@gmail.com>
2025-06-04 19:02:27 -0400

    remove vendored-ssl feature

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,12 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
 
-# build statically linked binary with Rust
-if [ "$(uname)" = "Darwin" ]; then
-    cargo install --features vendored-openssl --no-track --locked --root "$PREFIX" --path ./cli --bin jj jj-cli
-else
-    cargo install --no-track --locked --root "$PREFIX" --path ./cli --bin jj jj-cli
-fi
+cargo install --no-track --locked --root "$PREFIX" --path ./cli --bin jj jj-cli
 
 cargo-bundle-licenses \
     --format yaml \
